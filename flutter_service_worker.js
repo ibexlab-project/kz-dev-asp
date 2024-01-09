b20b6315dffd4271fc7458131782a20f7829a993
jeon dong hui <dhjeon@ibexlab.com>
2024-01-09 16:48:17 +0900

    0.1.137_24_01_09-1_D

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -996,11 +996,11 @@ const RESOURCES = {
 "icons/Icon-512.png": "ad354ff23c2c6aca0ab685a2847f3ded",
 "icons/Icon-maskable-192.png": "d5d28c85382a2658157ac5fb040622d2",
 "icons/Icon-maskable-512.png": "e28acd36096e6ed4cbcc4cae1fcae75d",
-"index.html": "f018e7dbf3552f4c5d898434c5c38ab6",
-"/": "f018e7dbf3552f4c5d898434c5c38ab6",
+"index.html": "c3bceec684b39930f04789fa68dd59e3",
+"/": "c3bceec684b39930f04789fa68dd59e3",
 "index_kr.html": "5aec52ee07ec894bf54adb2df6b6bece",
 "index_us.html": "d372f1532c01bde79873cc5df106b24a",
-"main.dart.js": "bec5e54d61be1f38f11da42e7d687536",
+"main.dart.js": "b95a5b7d6f07c9fcc8cc3f725283e72e",
 "manifest.json": "8a825e3be398a6793a13d96cd9154679",
 "version.json": "9295016edddcb8272bc119b445d04f47"
 };
