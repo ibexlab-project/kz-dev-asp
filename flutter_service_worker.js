a84194cb0c953be98b4213ede09281f8a99bd3d5
Choi Un Don <undon.choi@ibexlab.com>
2024-01-11 11:50:26 +0900

    0.1.137_24_01_11_D

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -996,11 +996,11 @@ const RESOURCES = {
 "icons/Icon-512.png": "ad354ff23c2c6aca0ab685a2847f3ded",
 "icons/Icon-maskable-192.png": "d5d28c85382a2658157ac5fb040622d2",
 "icons/Icon-maskable-512.png": "e28acd36096e6ed4cbcc4cae1fcae75d",
-"index.html": "e6698ef83e053453b917044cdc0e8770",
-"/": "e6698ef83e053453b917044cdc0e8770",
+"index.html": "bbda216046b1e435783967ec8ac79544",
+"/": "bbda216046b1e435783967ec8ac79544",
 "index_kr.html": "5aec52ee07ec894bf54adb2df6b6bece",
 "index_us.html": "d372f1532c01bde79873cc5df106b24a",
-"main.dart.js": "f426d1735ecd507e8cd1eabb05e204b2",
+"main.dart.js": "184fb67bd12f9eae35a8c256219246e6",
 "manifest.json": "8a825e3be398a6793a13d96cd9154679",
 "version.json": "9295016edddcb8272bc119b445d04f47"
 };
